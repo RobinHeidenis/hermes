b967e17a746b4f47c8cec8e3ec0fbca4f5c81ce9
Robin Heidenis <robin.heidenis@gmail.com>
2024-02-08 18:10:51 +0000

    fix: 🐛 groupby conditions for last month

--- src/hooks/useCalculateExpenseTotals.ts
+++ src/hooks/useCalculateExpenseTotals.ts
@@ -4,15 +4,28 @@ import type { RouterOutputs } from "~/utils/api";
 
 const groupByDate = ({ createdAt }: { createdAt: Date }) => {
   const date = dayjs(createdAt);
+  const startOfMonth = dayjs().startOf("month");
+
   if (date.isAfter(dayjs().add(1, "day"), "day")) return "Coming up";
   if (date.isSame(dayjs(), "day")) return "Today";
-  if (date.isSame(dayjs().subtract(1, "day"), "day")) return "Yesterday";
-  if (date.isAfter(dayjs().subtract(1, "week"), "week"))
-    return "Earlier this week";
-  if (date.isAfter(dayjs().subtract(2, "weeks"), "week")) return "Last week";
   if (
-    date.isAfter(dayjs().startOf("month").subtract(1, "month")) &&
-    date.isBefore(dayjs().startOf("month"))
+    date.isAfter(startOfMonth) &&
+    date.isSame(dayjs().subtract(1, "day"), "day")
+  )
+    return "Yesterday";
+  if (
+    date.isAfter(startOfMonth) &&
+    date.isAfter(dayjs().subtract(1, "week"), "week")
+  )
+    return "Earlier this week";
+  if (
+    date.isAfter(startOfMonth) &&
+    date.isAfter(dayjs().subtract(2, "weeks"), "week")
+  )
+    return "Last week";
+  if (
+    date.isAfter(startOfMonth.subtract(1, "month")) &&
+    date.isBefore(startOfMonth)
   )
     return "Last month";
   else return "Older";
@@ -21,10 +34,12 @@ const groupByDate = ({ createdAt }: { createdAt: Date }) => {
 type ExpenseItem = RouterOutputs["expense"]["getLast50Expenses"][number];
 
 type GroupedByDate = {
+  "Coming up": ExpenseItem[];
   Today: ExpenseItem[];
   Yesterday: ExpenseItem[];
-  "This week": ExpenseItem[];
+  "Earlier this week": ExpenseItem[];
   "Last week": ExpenseItem[];
+  "Last month": ExpenseItem[];
   Older: ExpenseItem[];
 };
 
@@ -41,17 +56,11 @@ export const useCalculateExpenseTotals = (
     (prev, curr) => prev + Number(curr.price),
     0,
   );
-  const expensesInPreviousMonth = expenses?.filter((expense) => {
-    const date = dayjs(expense.createdAt);
-    return (
-      date.isAfter(dayjs().subtract(2, "months").startOf("month")) &&
-      date.isBefore(dayjs().startOf("month"))
-    );
-  });
-  const totalSpentInPreviousMonth = expensesInPreviousMonth?.reduce(
-    (prev, curr) => prev + Number(curr.price),
-    0,
-  );
+  const totalSpentInPreviousMonth =
+    groupedExpenses["Last month"]?.reduce(
+      (prev, curr) => prev + Number(curr.price),
+      0,
+    ) ?? 0;
 
   return {
     groupedExpenses,
