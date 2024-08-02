1dace4a26c3c40e3a8019a058561ed62119597ce
Robin Heidenis <r.heidenis@hoorayhr.io>
2024-08-03 01:37:21 +0200

    feat: 🎸 set user to null when logging out

--- src/components/navigation/SignOutForm.tsx
+++ src/components/navigation/SignOutForm.tsx
@@ -1,11 +1,15 @@
 import { useRouter } from "next/router";
 import type { PropsWithChildren } from "react";
 import { api } from "~/utils/api";
+import { useSetAtom } from "jotai";
+import { userAtom } from "~/utils/userAtom";
 
 export const SignOutForm = ({ children }: PropsWithChildren) => {
+  const setUser = useSetAtom(userAtom);
   const { push } = useRouter();
   const { mutate } = api.auth.logout.useMutation({
     onSuccess: () => {
+      setUser(null);
       void push("/auth/login");
     },
   });
